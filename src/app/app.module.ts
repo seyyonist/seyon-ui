4145ec561553cc6c5059a8b170652f835a50ed16
bbroko <bbroko@gmail.com>
2018-04-06 20:35:15 +0530

    Basic UI Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
+const appRoutes: Routes = [
+  
+  { path: 'dashboard', component: DashboardComponent },
+  /*{ path: 'maintenance', component: MaintenanceComponent },
+  { path: 'expence', component: ExpenceComponent },
+  { path: 'monthlyReport', component: MonthlyReportComponent },
+  { path: 'flatsReport', component: FlatsReportComponent }
+  */
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
