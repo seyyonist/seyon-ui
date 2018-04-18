336625710e54f8478f532f3a5fd994a1c5a9e939
bbroko <bbroko@gmail.com>
2018-04-18 20:19:43 +0530

    Updated UI

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Client} from './client.domain';
+
+import {ClientService} from './client.service';
 
 @Component({
   selector: 'app-client',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientComponent implements OnInit {
 
-  constructor() { }
+  clients:Client[]=[];
+  error:boolean=false;
+  errorMessage:string="";
+  client:Client=new Client();
+  constructor(private clientService:ClientService) { }
 
   ngOnInit() {
+    this.getClients();
   }
 
+  getClients():void{
+    this.clientService.getForCompany()
+    .subscribe(
+      clients=>{
+        this.clients=clients
+      },
+      err=>{
+       console.log(err);
+       this.error=true;
+       this.errorMessage=err;
+      }
+    )
+  }
+
+  edit(client:Client):void{
+    this.client=client;
+  }
+  new():void{
+    this.client=new Client();
+  }
+  submit():void{
+    alert("Submitted");
+  }
 }
