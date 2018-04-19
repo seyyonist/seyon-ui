67a23a6339a99f3cc05b194425a844c07be28784
bbroko <bbroko@gmail.com>
2018-04-19 16:00:03 +0530

    Client Form completed

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Client} from './client.domain';
+import { Client } from './client.domain';
 
-import {ClientService} from './client.service';
+import { ClientService } from './client.service';
 
 @Component({
   selector: 'app-client',
@@ -10,37 +10,73 @@ import {ClientService} from './client.service';
 })
 export class ClientComponent implements OnInit {
 
-  clients:Client[]=[];
-  error:boolean=false;
-  errorMessage:string="";
-  client:Client=new Client();
-  constructor(private clientService:ClientService) { }
+  clients: Client[] = [];
+  error: boolean = false;
+  errorMessage: string = "";
+  client: Client = new Client();
+  success:boolean=true;
+
+  constructor(private clientService: ClientService) { }
 
   ngOnInit() {
     this.getClients();
   }
 
-  getClients():void{
+  getClients(): void {
+    this.success=false;
+    this.error = false;
     this.clientService.getForCompany()
-    .subscribe(
-      clients=>{
-        this.clients=clients
+      .subscribe(
+      clients => {
+        this.clients = clients
       },
-      err=>{
-       console.log(err);
-       this.error=true;
-       this.errorMessage=err;
+      err => {
+        console.log(err);
+        this.error = true;
+        this.errorMessage = err.error.message;
       }
-    )
+      )
   }
 
-  edit(client:Client):void{
-    this.client=client;
+  edit(client: Client): void {
+    this.client = client;
   }
-  new():void{
-    this.client=new Client();
+  new(): void {
+     this.success=false;
+    this.error = false;
+    this.client = new Client();
   }
-  submit():void{
-    alert("Submitted");
+  submit(): void {
+    this.success=false;
+    this.error = false;
+    this.clientService.save(this.client)
+      .subscribe(
+      client => {
+        this.client = client
+        this.getClients();
+        this.success=true;
+      },
+      err => {
+        console.log(err);
+        this.error = true;
+        this.errorMessage = err.error.message;
+      }
+      )
+  }
+
+  changeListener($event): void {
+    this.readThis($event.target);
+  }
+
+  readThis(inputValue: any): void {
+    if (inputValue.files.length === 0) {
+      return;
+    }
+    var file: File = inputValue.files[0];
+    var myReader: FileReader = new FileReader();
+    myReader.onloadend = (e) => {
+      this.client.logoImg = myReader.result;
+    }
+    myReader.readAsDataURL(file);
   }
 }
