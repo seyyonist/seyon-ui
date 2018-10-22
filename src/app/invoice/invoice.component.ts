da8301dce61c066b3648dfd1c04d81652792742f
bbroko <bbroko@gmail.com>
2018-10-22 19:57:59 +0530

    pring invoice with GST manupulation

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -191,6 +191,10 @@ export class InvoiceComponent implements OnInit {
 
 
   savePerformaInvoice(): void {
+    if(!this.selClientId){
+      alert("please select the client");
+      return;
+    }
     this.success = false;
     this.error = false;
     this.invoiceData.invoice = this.invoice;
@@ -219,6 +223,7 @@ export class InvoiceComponent implements OnInit {
     this.invoiceService.cancel(this.invoiceData.invoice.id).subscribe(
       invoice => {
         this.invoiceData.invoice = invoice;
+        this.invoice.status="CANCELED";
         this.success = true;
       },
       err => {
