15516369bcefb73d4c361f9caada317c6770c05a
vijaykarthik n <nvijaykarthik@gmail.com>
2019-04-04 20:33:40 +0530

    modify menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Company } from './company/company.domain';
+import { CompanyService } from './company/company.service';
+import { CompanyGlobalVar } from './globals';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  company: Company = new Company();
+
+  constructor(private companyService: CompanyService,private companyGlobalVar: CompanyGlobalVar){}
+
+  ngOnInit() {
+
+    console.debug("Inside NavComponent init");
+    this.companyService.getCompany()
+    .subscribe(
+    company => {
+      this.company = company;
+      this.companyGlobalVar.companyName=company.companyName;
+      this.companyGlobalVar.ownerName=company.ownerName;
+      this.companyGlobalVar.state=company.state;
+      this.companyGlobalVar.pinCode=company.pinCode;
+      this.companyGlobalVar.phonePrimary=company.phonePrimary;
+      this.companyGlobalVar.tanNo=company.tanNo;
+      this.companyGlobalVar.gstNo=company.gstNo;
+      this.companyGlobalVar.panNo=company.panNo;
+      this.companyGlobalVar.primaryEmail=company.primaryEmail;
+    },
+    err => {
+      console.error("Not able to set companyGlobalVar");
+    }
+    )
+  }
+
 }
